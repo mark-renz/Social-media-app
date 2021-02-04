3ab64f71ec8c77ab3e52d8fec3129d2255b8c791
mark-renz <markrenzmanalo@gmail.com>
2021-02-04 22:45:05 +0800

    added delete comment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const {ApolloServer} = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const typeDefs = require('./graphql/typeDefs')
+const typeDefs = require('./graphql/typeDefs');
 const { MONGODB } = require('./config.js');
-const resolvers = require('./graphql/resolvers')
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
     typeDefs,
@@ -11,10 +11,14 @@ const server = new ApolloServer({
     context: ({ req }) => ({ req })
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> {
+mongoose.connect(MONGODB, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+.then(() => {
     console.log('MONGODB Connected');
-  return server.listen({port: 5000});
-}).then(res => {
-    console.log(`Server running at ${res.url}`)
+    return server.listen({port: 5000});
+})
+.then(res => {
+    console.log(`Server running at ${res.url}`);
 })
