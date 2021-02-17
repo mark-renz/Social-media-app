377406327a804e48667d7b287a32e9ea845f1045
mark-renz <markrenzmanalo@gmail.com>
2021-02-17 20:56:40 +0800

    update server for deployment

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const resolvers = require('./graphql/resolvers');
 
 const pubsub = new PubSub();
 
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -19,8 +21,11 @@ mongoose.connect(MONGODB, {
 })
 .then(() => {
     console.log('MONGODB Connected');
-    return server.listen({port: 5000});
+    return server.listen({port: PORT});
 })
 .then(res => {
     console.log(`Server running at ${res.url}`);
+})
+.catch(err => {
+    console.error(err);
 })
